903b25bf67d5a1e6c0c7b6760bf6bea45fda44c6
as7ronau7 <scott@scott-nguyen.com>
2024-08-01 12:38:43 -0500

    Implement basic working calculator function

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
-let displayData = "";
-let firstNum;
-let secondNum;
-let operand;
-let result;
-
-// grab display
+// get display element
 const display = document.querySelector('.display');
 
-// grab buttons
-const numBtns = document.querySelectorAll('.btn-num');
-const operators = document.querySelectorAll('.btn-operate');
+// get button HTMLcollection
+const buttons = document.querySelectorAll('.btn-num');
 
+//
+let displayData = '';
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+let calculation;
 
+const clearBtn = document.querySelector('.btn-clear');
+clearBtn.addEventListener('click', () => {
+    console.log('clicked')
+    display.textContent = '';
+    displayData = '';
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+})
+// add function
+const add = (num1, num2) => num1 + num2;
+// subtract function
+const subtract = (num1, num2) => num1 - num2;
+// multiply function
+const multiply = (num1, num2) => num1 * num2;
+// divide function
+const divide = (num1, num2) => num1 / num2;
 
-const calculator = {
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
-    subtract: (num1, num2) => {
-        return num1 - num2;
-    },
-    divide: (num1, num2) => {
-        return num1 / num2;
-    },
-    multiply: (num1, num2) => {
-        return num1 * num2;
-    },
-    clear: () => {
-        display.textContent = '';
-        displayData = '';
+const calcResults = (num1, num2, operation) => {
+    if (operator === '+') {
+        calculation = add(num1, num2);
+        return calculation
+    } else if (operator === '-') {
+        calculation = subtract(num1, num2);
+        return calculation
+    } else if (operator === '*') {
+        calculation = multiply(num1, num2);
+        return calculation
+    } else if (operator === '/') {
+        calculation = divide(num1, num2);
+        return calculation
     }
+    
 }
 
-numBtns.forEach(button => {
+// event listener to get button value
+buttons.forEach(button => {
     button.addEventListener('click', () => {
+        console.log(`${button.value}`);
         displayData += `${button.value}`;
         display.textContent = displayData;
-        firstNum = Number.parseInt(displayData);   
+
+        switch(`${button.value}`) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                console.log(`operator: ${button.value} selected`);
+                firstNum = parseInt(displayData);
+                operator = `${button.value}`;
+                break;
+            case '=':
+                secondNum = parseInt(displayData.replace(/^.*[+\-*\/]/, ''));
+                display.textContent = calcResults(firstNum, secondNum, operator);
+                break;
+        }
     })
 })
-
 
 
